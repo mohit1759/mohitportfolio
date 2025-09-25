b37033a9c2719fdbc935ad6d6c7f997b09c3aeef
Mohit <mohitsoni93556555@gmail.com>
2025-09-25 22:37:15 +0530

    updates
    ok

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -49,6 +50,7 @@ export default function RootLayout({ children }) {
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
